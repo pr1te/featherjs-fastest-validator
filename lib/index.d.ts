ceffb141d014b61c1b4970b17f81eb7a8de39036
Jugkapong Pooban <j.pooban@gmail.com>
2019-12-16 11:18:49 +0700

    fix(index.d.ts): resolve typescript support

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,16 @@
+import { HookContext } from '@feathersjs/feathers';
 import { ValidationSchema } from 'fastest-validator';
 
 declare module 'feathers-fastest-validator' {
 
+  export default function validator(schema: ValidationSchema, data: Data): HookContext;
+
   interface Schema extends ValidationSchema {}
 
-  type Field = string;
+  type Data = string;
 
   export {
     Schema,
-    Field,
+    Data,
   };
 }
