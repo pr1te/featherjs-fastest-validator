f555ebe741616e344a45878e3ba4b98fba8036f5
Jugkapong Pooban <j.pooban@gmail.com>
2022-05-14 03:55:10 +0700

    feat: extended fastest validator class instead

--- test/index.test.js
+++ test/index.test.js
@@ -4,14 +4,19 @@
 
 const test = require('ava');
 const feathers = require('@feathersjs/feathers');
-const validator = require('../lib');
+const { BadRequest } = require('@feathersjs/errors');
+const FastestValidator = require('fastest-validator');
 
+const Validator = require('../lib');
 
 test.beforeEach((t) => {
-  /** Create new plain feathers application */
-  t.context = { app: feathers() };
+  // create new plain feathers application
+  t.context.app = feathers();
 
-  /** Register the dummies service */
+  // create validate instance
+  t.context.validator = new Validator();
+
+  // register the dummies service
   t.context.app.use('/dummies', {
     async create (data) {
       if (!data) throw new Error('data is undefined');
@@ -20,128 +25,123 @@ test.beforeEach((t) => {
   });
 });
 
-
-test('the type of \'feathers-fastest-validator\' should be a function', (t) => {
-  /** Arrange, Act */
-  const type = typeof validator;
-
-  /** Assert */
-  t.is(type, 'function');
+test('should be extended from \'FastestValidator\' instance', (t) => {
+  t.true(Validator.prototype instanceof FastestValidator)
 });
 
-
 test('must THROW an error, if using \'feathers-fastest-validator\' hooks as AFTER hooks', async (t) => {
-  /** Arrange: assign validate hooks to after hooks of dummies service */
-  const { app } = t.context;
+  // arrange: assign validate hooks to after hooks of dummies service
+  const { app, validator } = t.context;
+
+  const schema = {
+    key: { type: 'string' },
+  };
 
   app.service('dummies').hooks({
     after: {
-      create: [
-        validator({
-          key: { type: 'string' },
-        }),
-      ],
+      create: [ validator.validateHook(schema) ],
     },
   });
 
-  /** Act */
+  // act
   const error = await t.throwsAsync(async () => {
     await app.service('dummies').create({ key: 'value' });
-  }, Error);
+  }, { instanceOf: Error });
 
-  /** Assert */
+  // assert
   t.is(error.message, 'The \'feathers-fastest-validator\' hook can only be used as a \'before\' hook.');
 });
 
 
 test('must THROW an error, if using \'feathers-fastest-validator\' hooks as ERROR hooks', async (t) => {
-  /** Arrange: assign validate hooks to error hooks of dummies service */
-  const { app } = t.context;
+  // arrange: assign validate hooks to error hooks of dummies service
+  const { app, validator } = t.context;
+
+  const schema = {
+    key: { type: 'string' },
+  };
 
   app.service('dummies').hooks({
     error: {
-      create: [
-        validator({
-          key: { type: 'string' },
-        }),
-      ],
+      create: [ validator.validateHook(schema) ],
     },
   });
 
-  /** Act */
+  // act
   const error = await t.throwsAsync(async () => {
     await app.service('dummies').create(undefined);
-  }, Error);
+  }, { instanceOf: Error });
 
-  /** Assert */
+  // assert
   t.is(error.message, 'The \'feathers-fastest-validator\' hook can only be used as a \'before\' hook.');
 });
 
 
 test('must NOT THROW an error, if using \'feathers-fastest-validator\' hooks as BEFORE hooks', async (t) => {
-  /** Arrange: assign validate hooks to after hooks of dummies service */
-  const { app } = t.context;
+  // arrange: assign validate hooks to after hooks of dummies service
+  const { app, validator } = t.context;
+
+  const schema = {
+    key: { type: 'string' },
+  };
 
   app.service('dummies').hooks({
     before: {
-      create: [
-        validator({
-          key: { type: 'string' },
-        }),
-      ],
+      create: [ validator.validateHook(schema) ],
     },
   });
 
-  /** Act */
+  // act
   const promise = app.service('dummies').create({ key: 'value' });
 
-  /** Assert */
+  // assert
   await t.notThrowsAsync(promise);
 });
 
 
 test('must THROW an error, if validate the data failed', async (t) => {
-  /** Arrange: assign validate hooks to error hooks of dummies service */
-  const { app } = t.context;
+  // arrange: assign validate hooks to error hooks of dummies service
+  const { app, validator } = t.context;
+
+  const schema = {
+    key: { type: 'number' },
+  };
 
   app.service('dummies').hooks({
     before: {
-      create: [
-        validator({
-          key: { type: 'number' },
-        }),
-      ],
+      create: [ validator.validateHook(schema) ],
     },
   });
 
-  /** Act */
+  // act
   const error = await t.throwsAsync(async () => {
     await app.service('dummies').create({ key: 'value' });
-  }, Error);
+  }, { instanceOf: BadRequest });
 
-  /** Assert */
+  // assert
   t.is(error.message, 'Validation failed');
 });
 
 
 test('return data, if use \'feathers-fastest-validator\' hooks as before hooks and pass the validation', async (t) => {
-  /** Arrage: assign validate hooks to before hook and set up the ValidationSchema to validate data */
-  const { app } = t.context;
+  // arrange: assign validate hooks to before hook and set up the ValidationSchema to validate data
+  const { app, validator } = t.context;
+
+  const schema = {
+    key: { type: 'string' },
+  };
 
   app.service('dummies').hooks({
     before: {
-      create: [
-        validator({
-          key: { type: 'string' },
-        }),
-      ],
+      create: [ validator.validateHook(schema) ],
     },
   });
 
-  /** Act */
+  // act
   const promise = app.service('dummies').create({ key: 'value' });
 
-  /** Assert */
+  // assert
   await t.notThrowsAsync(promise);
+
   t.deepEqual(await promise, { key: 'value' });
 });
